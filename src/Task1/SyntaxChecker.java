b0780bccfc05b03d8b3bda7e1ac53632f124ff5a
Karistin Simbolon <karistinsimbolon@gmail.com>
2025-09-07 20:35:53 -0700

    Handson java syntax practice

---
+++ src/Task1/SyntaxChecker.java
@@ -0,0 +1,14 @@
+package Task1;
+
+public class SyntaxChecker {
+    public static void main(String[] args) {
+        // Coba identifikasi kesalahan dalam kode berikut
+        int Number2 = 10;
+        String firstname = "Budi";
+        int classNumber = 25;
+        System.out.println("Hello World");
+        boolean isActive = true;
+        double nilairata = 85.5;
+    }
+}
+
